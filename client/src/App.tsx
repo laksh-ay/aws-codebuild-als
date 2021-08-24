49959132713c2f70687af4435457d70512603111
shreyrohatgi <rohatgishrey@gmail.com>
2021-08-24 11:41:38 +0530

    CHECK

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import axios from "axios"
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import axios from 'axios'
+import './App.css'
 
 function App() {
-
-  const [apple, setApple] = useState<string>("")
+  const [apple, setApple] = useState<string>('')
 
   const clickHandler = async () => {
     const res = await axios.get(`${process.env.REACT_APP_GW_URL}/hello`)
@@ -13,16 +12,23 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-        <a onClick={clickHandler} style={{cursor: "pointer", textDecorationLine: "underline"}}>Click me</a> to check if lambda is working or knot
+          <a
+            onClick={clickHandler}
+            style={{ cursor: 'pointer', textDecorationLine: 'underline' }}
+          >
+            Click me
+          </a>{' '}
+          to check if lambda is working or knot
         </p>
         <p>{apple}</p>
+        <p>{`${process.env.ApiUrl}`}</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
