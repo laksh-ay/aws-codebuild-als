3cd35cbd75a2d568716ea9167f60f645e686702f
Lakshay Khurana <lakshay.khurana@block8.com>
2021-08-26 17:28:36 +0530

    stage changed

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 const serverless = require('serverless-http')
 const express = require('express')
+import * as dotenv from "dotenv";
 const app = express()
+dotenv.config();
 
 app.get('/hello', (req, res) => {
   res.send(`Hello World, You Suck! ${process.env.QA_TITLE}`)
