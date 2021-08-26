41918d7a3ec9e13d5eaa92bed858c86033d7d819
Lakshay Khurana <lakshay.khurana@block8.com>
2021-08-26 17:34:40 +0530

    hiugjyvj

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,7 @@
 const serverless = require('serverless-http')
 const express = require('express')
-import * as dotenv from "dotenv";
 const app = express()
-dotenv.config();
+require('dotenv').config()
 
 app.get('/hello', (req, res) => {
   res.send(`Hello World, You Suck! ${process.env.QA_TITLE}`)
