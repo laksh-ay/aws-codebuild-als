b2d87cd5a4a2f8f0f55ff21f91833115d8d3c5a4
Lakshay Khurana <lakshay.khurana@block8.com>
2021-08-26 16:44:31 +0530

    Did Something

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/hello', (req, res) => {
-  res.send('Hello World, You Suck!')
+  res.send(`Hello World, You Suck! ${process.env.QA_TITLE}`)
 })
 
 module.exports.handler = serverless(app)
