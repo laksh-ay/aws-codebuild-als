a4c78edf6da036a8dcaa08eef8355fa4f09ead2d
Lakshay Khurana <lakshay.khurana@block8.com>
2021-08-27 14:28:27 +0530

    biggest update ever

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const app = express()
 require('dotenv').config()
 
 app.get('/hello', (req, res) => {
-  res.send(`Hello World, You Suck! ${process.env.QA_TITLE}`)
+  res.send(`Hello World, You Suck! ${process.env.TITLE} ${process.env.DESCRIPTION} ${process.env.BODY} ${process.env.SIGN}`)
 })
 
 module.exports.handler = serverless(app)
